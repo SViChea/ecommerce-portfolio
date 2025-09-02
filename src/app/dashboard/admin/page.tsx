03aa83d6325d082983e898340fe99d76494a5bfc
ViChea <ssovicheasok@gmail.com>
2025-09-02 12:03:48 +0700

    Rename File Dashboard/admin

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,18 +1,16 @@
-'use client'
-import { columnsCart } from '@/components/dashboard/ColumnProduct'
-import TableCartComponent from '@/components/dashboard/TableCartComponent'
-import { useGetAllProductsQuery } from '@/redux/services/products/productsApi';
-import { ProductType } from '@/types/productType';
+"use client";
+import { columnsCart } from "@/components/dashboard/ColumnProduct";
+import TableCartComponent from "@/components/dashboard/TableCartComponent";
+import { useGetAllProductsQuery } from "@/redux/services/products/productsApi";
+import { ProductType } from "@/types/productType";
 
-export default function AdminPage() {
-    const { data: product, isLoading } = useGetAllProductsQuery();
-  
-    if(isLoading){
-      return <div>loading...</div>
-    }
-    const items = (product as ProductType[]) || [];
-    console.log(items)  
-  return (
-    <TableCartComponent columns={columnsCart} data={items}/>
-  )
+function Data(): ProductType[] {
+  const { data: product } = useGetAllProductsQuery();
+  const items = (product as ProductType[]) || [];
+  console.log(items);
+  return items;
+}
+
+export default function page() {
+  return <TableCartComponent columns={columnsCart} data={Data()} />;
 }
